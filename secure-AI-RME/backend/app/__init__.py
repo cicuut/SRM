efde242bfa28186c17e0c0390296a2cc108d4346
msqueeniquee <184899658+msqueeniquee@users.noreply.github.com>
2026-04-27 20:56:02 +0700

    update init.py

--- secure-AI-RME/backend/app/__init__.py
+++ secure-AI-RME/backend/app/__init__.py
@@ -6,27 +6,32 @@ from datetime import timedelta
 import os
 from dotenv import load_dotenv
 
+
 db = SQLAlchemy()
+
 
 def create_app():
     load_dotenv()
+
     app = Flask(__name__)
 
-    CORS(app) 
-    
+    CORS(app)
+
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET')
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=1)
-    
-    
+
     db.init_app(app)
     jwt = JWTManager(app)
-    
+
     from .routes.auth import auth_bp
     from .routes.medical_record import medical_record_bp
     from .routes.visit_report import visit_report_bp
+    from .routes.financial import financial_bp
+
     app.register_blueprint(auth_bp, url_prefix='/api/auth')
     app.register_blueprint(medical_record_bp, url_prefix='/api/medical-record')
     app.register_blueprint(visit_report_bp, url_prefix='/api/visit-report')
-    
+    app.register_blueprint(financial_bp, url_prefix='/api/financial')
+
     return app
